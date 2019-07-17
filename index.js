953bb4487729ce1c2d587bc4e219aec2f1cf8f66
Andrea Toston <tostonar@gmail.com>
2019-07-17 14:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return intros;
 }
+
+function johnLennonFacts(facts) {
+  var counter = facts.length;
+  var result = [];
+  while (counter > 0) {
+    result.push(`${facts[counter-1]}!!!`);
+    counter --;
+  }
+  return result;
+}
