65fabc2c9dda251fd86cba48e5f07f24f11f062f
Andrea Toston <tostonar@gmail.com>
2019-07-17 14:19:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var intros = [];
+  for (var i = 0; i < musicians.length; i++) {
+    intros.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return intros;
+}
